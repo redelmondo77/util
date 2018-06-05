37510e4bdff5f02604bad20bc38628f56eca158b
redelmondo77 <redelmondo77@users.noreply.github.com>
2018-06-06 01:12:58 +0200

    start project

---
+++ applicazione/src/main/java/it/applicazione/ApplicazioneApplication.java
@@ -0,0 +1,12 @@
+package it.applicazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazioneApplication.class, args);
+	}
+}
---
+++ applicazione/src/test/java/it/applicazione/ApplicazioneApplicationTests.java
@@ -0,0 +1,16 @@
+package it.applicazione;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicazioneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
